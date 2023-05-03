2a92b7ed779047a23d04e27a5362869628b251e6
Gleydson José <gleydson_jose333@outlook.com>
2023-05-03 18:30:28 -0300

    Aula sobre softdelete

--- database/migrations/2023_05_03_181121_alter_fornecedores_softdelete.php
+++ database/migrations/2023_05_03_181121_alter_fornecedores_softdelete.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('fornecedors', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('nome', 50);
+        //
+        Schema::table('fornecedors', function (Blueprint $table) {
             $table->softDeletes();
         });
     }
@@ -24,6 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('fornecedors');
+        //
+        Schema::table('fornecedors', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
     }
 };
